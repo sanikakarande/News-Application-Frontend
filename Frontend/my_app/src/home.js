c4ccbcb24a7a143cbc34c43dce85cfe442de8bff
Sanika <sanikak323@gmail.com>
2025-01-08 12:05:09 +0530

    Your message here

---
+++ Frontend/my_app/src/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container my-5">
+      <h1>Welcome to NewsApp</h1>
+      <p>Stay updated with the latest news from around the world.</p>
+    </div>
+  );
+};
+
+export default Home;
